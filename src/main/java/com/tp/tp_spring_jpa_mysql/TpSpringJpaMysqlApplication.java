af8e161056302ac94f9fa5a7103a5e20571e0785
Nicolas Gaget <n.gaget69@gmail.com>
2021-06-15 21:45:10 +0200

    initialisation spring / lombok / mysql / jpa

---
+++ src/main/java/com/tp/tp_spring_jpa_mysql/TpSpringJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.tp.tp_spring_jpa_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringJpaMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringJpaMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tp/tp_spring_jpa_mysql/TpSpringJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.tp_spring_jpa_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringJpaMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
